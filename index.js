7d4723bda337c7eb6799830f94e08eba0b196af2
mdhainf20 <mdhanif115825@gmail.com>
2021-11-01 22:38:54 +0600

    remove some console

--- index.js
+++ index.js
@@ -12,7 +12,6 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.8f4kw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
-console.log(uri);
 
 async function run(){
     try{
